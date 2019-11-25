e99d631063f40e74f844dc24d4368c7fc4ff0dbc
Scott Dexter <dextersd@alma.edu>
2019-11-25 11:23:07 -0500

    Add Counter

---
+++ Counter.java
@@ -0,0 +1,18 @@
+package edu.alma.CSC230.counter;
+
+public class Counter {
+
+	private int count;
+
+	public Counter() {
+		count = 0;
+}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void increment() {
+		count++;
+	}
+}
